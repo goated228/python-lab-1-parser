c6a6537575a1232680c731dc3cabc1e8583c1bbb
A$APk <const.qqdae@gmail.com>
2026-05-18 01:25:46 +0600

    added parsing

--- parser.py
+++ parser.py
@@ -10,3 +10,22 @@ def get_page():
         return response.text
 
     return None
+
+def parse_birth_data(html):
+    soup = BeautifulSoup(html, "html.parser")
+
+    stats = soup.find_all("div", {"class": "counter"})
+
+    result = []
+
+    for item in stats[:5]:
+        title = item.find("div", class_="counter_title")
+        value = item.find("span", class_="counter_number")
+
+        if title and value:
+            result.append({
+                "title": title.text.strip(),
+                "value": value.text.strip()
+            })
+
+    return result
